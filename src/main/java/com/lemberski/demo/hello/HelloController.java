5b0aaf7a3da0650e2f0b8b25cfa0f63183a74c82
thepierrre <piotr.owczarczyk98@gmail.com>
2024-11-06 15:56:23 +0100

    add resources

--- src/main/java/com/lemberski/demo/hello/HelloController.java
+++ src/main/java/com/lemberski/demo/hello/HelloController.java
@@ -1,14 +1,18 @@
 package com.lemberski.demo.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${name:World}")
+    private String name;
+
     @GetMapping
     public String sayHello() {
-        return "Hello World!";
+        return "Hello " + name + "!";
     }
 
 }
